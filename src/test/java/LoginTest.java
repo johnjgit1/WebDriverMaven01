73a1910d3eab56bc53d4be08b0ec9d5895a2390c
unknown <johnson1324@comcast.net>
2014-10-24 22:02:38 -0700

    initial commit

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+
+//lecture 129  github continued with lecture 130
+public class LoginTest {
+	
+	public static WebDriver driver;
+	// testng jars
+	@BeforeSuite
+	
+	public void setUp(){
+		
+		driver = new FirefoxDriver();
+		
+	}
+	@Test
+	public void doLogin(){
+		
+		driver.get("Http://gmail.com");	
+		driver.findElement(By.id("Email")).sendKeys("trainer@way2automation.com");
+		driver.findElement(By.id("Passwd")).sendKeys("abdc");
+	}
+	@AfterSuite 
+	
+	public void tearDown() {
+		//Thread.sleep(2000);
+		driver.quit();
+	}
+
+}
